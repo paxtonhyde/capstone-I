acf95374d816e320c271eeaa6b9782e48bcf3dc4
paxbhyde <34036930+paxbhyde@users.noreply.github.com>
2020-01-10 11:30:00 -0700

    Added a bootstrap/nonbootstrap param for hypothesis test

--- src/hypothesis_test.py
+++ src/hypothesis_test.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,6 +15,17 @@ DATA_DIRECTORY = os.path.join(HOME_DIRECTORY, "data")
 IMAGE_DIRECTORY = os.path.join(HOME_DIRECTORY, "images")
 
 if __name__ == "__main__":
+    ## argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', default="bootstrap", \
+                         type=str, help='Bootstrap individual samples from country means (bootstrap) \
+                         or use the country-level means only (mean). Default : bootstrap')
+    args = vars(parser.parse_args())
+    if args['mode'] == "mean":
+        do_bootstrap = False
+    else:
+        do_bootstrap = True
+
     path = f"{DATA_DIRECTORY}/country_preferences.csv"
     by_country = pd.read_csv(path)
     cluster = pd.read_csv(f"{DATA_DIRECTORY}/country_cluster_map.csv")
@@ -32,6 +44,8 @@ if __name__ == "__main__":
     fig, axes = plt.subplots(3, 1, figsize=(8,9))
     fig2, ax2 = plt.subplots()
 
+    values = np.array([])
+    file_suffix = ""
     ## plotting
     for cluster in [0,1,2]:
         group = by_country[by_country["cluster"] == cluster]
@@ -39,23 +53,28 @@ if __name__ == "__main__":
         total_responses = int(np.sum(group[f"n_{factor}"]))
         country_weights = np.array(group[f"n_{factor}"] / total_responses)
 
-        mu, std = np.mean(means), np.std(means)
-        bootstrap = np.random.choice(means, size=total_responses, p=country_weights)
+        if do_bootstrap:
+            values = np.random.choice(means, size=total_responses, p=country_weights)
+            file_suffix = "booted"
+        else:
+            values = means
 
         ax = axes[cluster]
-        x = np.linspace(np.min(means) - .1, np.max(means) + .1, 100)
+        x = np.linspace(np.min(values) - .1, np.max(values) + .1, 100)
+        ax.set_xlim(np.min(x) - .1, np.max(x) + .1)
+        ax.set_xlabel(f"Probability of choosing default {factor}")
+
+        ax.hist(values, density=True, label=f"density (n = {len(means)})")
+        mu, std = np.mean(values), np.std(values)
+
         ax.plot(x, stats.norm.pdf(x, loc=mu, scale=std), 'r-', alpha=0.8,\
                 label=f"normal distribution")
-        ## bootstrapped
-        ax.hist(bootstrap, density=True, label=f"density (n = {len(means)})")
         ax.set_yticklabels([])
-        ax.set_xlabel(f"Probability of choosing default {factor}")
-        ax.set_xlim(np.min(x) - .1, np.max(x) + .1)
         ax.set_title(f"{cluster_stats.cluster_name[cluster]}")
         ax.legend(loc='best')
 
         fig.tight_layout(pad=1)
-        fig.savefig(f"{IMAGE_DIRECTORY}/{factor}_histsbooted.png", dpi=120)
+        fig.savefig(f"{IMAGE_DIRECTORY}/{factor}_hists{file_suffix}.png", dpi=120)
 
         x = np.linspace(0, 1, 100)
         ax2.plot(x, stats.norm.pdf(x, loc=mu, scale=std), f'{cluster_stats.color[cluster]}-', alpha=0.8,\
@@ -67,13 +86,15 @@ if __name__ == "__main__":
         ax2.legend(loc='best')
 
         fig2.tight_layout(pad=1)
-        fig2.savefig(f"{IMAGE_DIRECTORY}/{factor}_distributionsbooted.png", dpi=120)
+        fig2.savefig(f"{IMAGE_DIRECTORY}/{factor}_distributions{file_suffix}.png", dpi=120)
 
     ## retrieving p values
     cluster_compared_p = np.zeros((3,3))
 
+    values, valuesnext = np.array([]), np.array([])
     for cluster, nextcluster in zip([0, 1, 2], [1, 2, 0]):
         b = (cluster + 1) % 3
+
         group = by_country[by_country["cluster"] == cluster]
         groupnext = by_country[by_country["cluster"] == nextcluster]
         means = np.array(group[f"p_{factor}"])
@@ -83,19 +104,25 @@ if __name__ == "__main__":
         country_weights = np.array(group[f"n_{factor}"] / total_responses)
         country_weightsnext = np.array(groupnext[f"n_{factor}"] / total_responsesnext)
 
-        bootstrap = np.random.choice(means, size=total_responses, p=country_weights)
-        bootstrapnext = np.random.choice(meansnext, size=total_responsesnext, p=country_weightsnext)
-        ## bootstrapped
-        t, p = stats.ttest_ind(bootstrap, bootstrapnext, equal_var=False)
-        cluster_compared_p[cluster][nextcluster] = round(p, 5)
-        cluster_compared_p[nextcluster][cluster] = round(p, 5)
+        if do_bootstrap:
+            values = np.random.choice(means, size=total_responses, p=country_weights)
+            valuesnext = np.random.choice(meansnext, size=total_responsesnext, p=country_weightsnext)
+            file_suffix = "booted"
+        else:
+            values, valuesnext = means, meansnext
+
+        
+        #print(f"Variances : ({cluster} : {np.var(values)}), ({nextcluster} : {np.var(valuesnext)}) ")
+        t, p = stats.ttest_ind(values, valuesnext, equal_var=False)
+        cluster_compared_p[cluster][nextcluster] = p
+        cluster_compared_p[nextcluster][cluster] = p
             
     significance_matrix = pd.DataFrame(data=cluster_compared_p, \
                                     index=['West', 'East', 'South'], \
                                     columns=['West', 'East', 'South'])
     table = tabulate(significance_matrix, headers=significance_matrix.columns, tablefmt='github')
 
-    tablefile = open(f"../data/tablebooted.txt", "w+")
+    tablefile = open(f"../data/table{file_suffix}.txt", "w+")
     for line in table:
         tablefile.write(line)
     
